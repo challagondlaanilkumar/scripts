87e499fabad089b2762a30e816ed941b4b8494e3
challagondlaanilkumar <challagondlaanilkumar@gmail.com>
2024-02-17 17:47:55 +0530

    docker and jenkins installtion

--- installastion-script.sh
+++ installastion-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERID=$(id -u)
 DATE=$(date +%F)
-LOG="java-installation-${DATE}.log"
+LOG="installation-${DATE}.log"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -21,32 +21,46 @@ if [ $USERID -ne 0 ]; then
 fi
 figlet -c updating
 sudo apt update -y &>>$LOG 
-
 VALIDATE $? "Updating apt"
-
 figlet -c java installation
-
 sudo apt install openjdk-17-jre -y &>>$LOG
-
 VALIDATE $? "installing java-17"
-
 echo "java -version"
-
 java -version 
-
-figlet -c updating
-
+#--------------------------------python3----------------------------------------------
+figlet -c Python3
 sudo apt update -y &>>$LOG 
-
 VALIDATE $? "Updating apt"
-
-figlet -c Python3 installation
-
 sudo apt install python3 -y &>>$LOG
-
 VALIDATE $? "Installing Python3..."
-
 echo "Python3 --version"
-
 python3 --version
+
+#------------------------------------docker--------------------------------------------
+figlet Docker
+sudo apt update -y &>>$LOG 
+VALIDATE $? "Updating apt"
+sudo apt install docker.io &>>$LOG
+VALIDATE $? "Installing Docker"
+sudo docker -v 
+
+#-----------------------------------Jenkins Using Docker--------------------------------
+figlet Jenkins
+sudo apt update -y &>>$LOG 
+VALIDATE $? "Updating apt"
+docker network create dwithi
+docker run -d --name jenkins --network=dwithi -p 8080:8080 jenkins/jenkins:lts &>>$LOG
+VALIDATE $? "Creating jenkins using docker image: jenkins/jenkins:lts "
+docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword &>>$LOG
+docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
+VALIDATE $? "cat /var/jenkins_home/secrets/initialAdminPassword"
+#-----------------------------------SonarQube Using Docker ------------------------------
+
+
+
+
+
+
+
+
 
